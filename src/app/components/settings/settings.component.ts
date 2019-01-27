c8fd73ced4208ef9ee2b2a6593c683fa6d14edcd
denis96z <zinoviev.denis@yandex.ru>
2019-01-27 15:51:22 +0300

    base settings added

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {UsersService} from '../../services/users.service';
+import {Router} from '@angular/router';
+import {UserUpdate} from '../../models/user-update';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +10,37 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  userUpdate: UserUpdate;
+  musicalInstruments: string;
 
-  constructor() {
+  constructor(private router: Router,
+              private authService: AuthService,
+              private usersService: UsersService) {
   }
 
   ngOnInit() {
+    if (!this.authService.sessionInfo) {
+      this.navigateToLogin();
+    }
+    this.usersService
+      .getUser(this.authService.sessionInfo.userId)
+      .subscribe(user => {
+        this.userUpdate = {
+          email: user.email,
+          fullName: user.fullName,
+          dateOfBirth: user.dateOfBirth,
+          musicalInstruments: user.musicalInstruments
+        };
+        this.musicalInstruments = user.musicalInstruments.join(',');
+      });
+  }
+
+  onBtnSubmitClick(): void {
+    console.log(this.userUpdate);
+    // TODO
+  }
+
+  private navigateToLogin(): void {
+    this.router.navigate(['/login']);
   }
 }
---
+++ src/app/models/user-update.ts
@@ -0,0 +1,6 @@
+export class UserUpdate {
+  email: string;
+  fullName: string;
+  dateOfBirth: Date;
+  musicalInstruments: Array<string>;
+}
