e6985c957605f466168dc802690f6b38480fc559
denis96z <zinoviev.denis@yandex.ru>
2019-01-27 15:27:54 +0300

    api service added

--- src/app/modules/app.module.ts
+++ src/app/modules/app.module.ts
@@ -1,6 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from '../components/app.component';
@@ -38,6 +39,7 @@ import {SettingsComponent} from '../settings/settings.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/services/api.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private backendURL = 'http://127.0.0.1:8000';
+  private baseURL = this.backendURL + '/api';
+
+  getBaseURL(): string {
+    return this.baseURL;
+  }
+}
